da4643c9e55602ce9730a4f2365075b2655d0337
ParasharDeb <parashardeb11@gmail.com>
2026-03-13 10:38:31 +0530

    starter code

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import cors from "cors"
+const app= express();
+app.use(express.json());
+app.use(cors())
+app.post("/",(req,res)=>{
+
+})
+app.get("/",(req,res)=>{
+
+})
+app.listen(3000)
