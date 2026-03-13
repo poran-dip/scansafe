ce71f8e8e8559ace138cc1838a012d0c2676bed5
Shivayan Chakravarty <shivayanchak@gmail.com>
2026-03-13 14:11:04 +0530

    error fix

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -4,5 +4,6 @@ export default [
   index("routes/home.tsx"),
   route("/auth", "routes/auth.tsx"),
   route("/check", "routes/trust.tsx"),
+  route("/fir", "routes/fir.tsx"),
   route("/dashboard", "routes/dashboard.tsx")
 ] satisfies RouteConfig;
